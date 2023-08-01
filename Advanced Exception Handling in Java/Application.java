57fcb537a2e9882fcc359547c80b61f0841468e6
Lera <valeriiakolesnikova21@gmail.com>
2023-08-01 13:06:52 +0500

    homework 3 added

--- Advanced Exception Handling in Java/Application.java
+++ Advanced Exception Handling in Java/Application.java
@@ -3,11 +3,20 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
-        try (FileReader reader =  new FileReader("/home/lk/Документы/reps/ExceptionsInProgrammingAndTheirHandling/ErrorHandlingInProgramming/Application.java");\
-        new FileWriter("test")) {
-            test.read();
+        try (FileReader reader =  new FileReader("/home/lk/Документы/reps/ExceptionsInProgrammingAndTheirHandling/ErrorHandlingInProgramming/Application.java");
+            FileWriter writer = new FileWriter("test")) {
+                while (reader.ready()){
+                    writer.write(reader.read());
+                }
         } catch (RuntimeException | IOException e) {
             System.out.println("catch exception " + e.getClass().getSimpleName());
         }
+
+        try {
+            System.out.println ("test operation");
+        } finally {
+            System.out.println ("finally operation");
+        }
+        System.out.println("Copy successfully");
     }
 }
